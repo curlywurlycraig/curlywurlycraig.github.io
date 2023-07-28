1733417efe095f8cde6ff7c8f129d69bed045ff0
Craig Wilkinson <craig@crwi.uk>
2023-07-28 14:09:14 +0100

    Use multiplication symbol

--- apps/numbers/src/index.tsx
+++ apps/numbers/src/index.tsx
@@ -37,7 +37,7 @@ const operatorString = {
   [Operator.DIV]: "÷",
   [Operator.SUB]: "–",
   [Operator.ADD]: "+",
-  [Operator.MUL]: "•"
+  [Operator.MUL]: "×"
 }
 
 const easyNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 15, 20];
